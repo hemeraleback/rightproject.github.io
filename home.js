de23ef1e1fe8d325d39758c4aa0aa180dadbb53d
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-06-30 20:57:37 +0200

    Update home.js

--- home.js
+++ home.js
@@ -2,4 +2,4 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     window.alert("Il sito non è ottimizzato per telefono, è consigliata la visione da Computer")
   }
 
-console.log("Creato da Right: hemeraleback & m4croxx, con l'aiuto di DFM - Marco")
+console.log("Creato da Right: hemeraleback & m4croxx, con l'aiuto di DFM - Marco", "color: #BD3434; font-size: 25px")
