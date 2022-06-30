6287983592fef8fd600ad3ed15a2fd907254f6d3
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-06-30 20:50:09 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,3 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    window.alert("Il sito non è ottimizzato per telefono, è consigliata la visione da Computer")
+  }
